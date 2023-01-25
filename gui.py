2c3d68e2dbd4ba8bc7552fd111e87957e83d770e
Alexandre <alexurluescu23@gmail.com>
2023-01-25 19:42:42 +0200

    gui: BUG detected
    The program doesn't recognize the coordonates from the active_sheet

--- gui.py
+++ gui.py
@@ -36,23 +36,63 @@ def parameters_selected_btn_clb():
     path = (path_label.cget("text"))
     email = input_email.get()
     password = input_password.get()
-    # print(sheet)
-    # print(path)
-    # print(email)
-    # print(password)
     return path
+
+
+def load_sheets_clb():
+    path = parameters_selected_btn_clb();
+    book = open_xlsx(path)
+    combobox.config(values=book.sheetnames)
 
 
 def open_xlsx(path):
     book = openpyxl.load_workbook(path, data_only=True)
+    # combobox.config(values=book.sheetnames)
     return book
+
+
+def email_emisor():
+    email = input_email.get()
+    return email
+
+
+def password_emisor():
+    password = input_password.get()
+    return password
+
+
+def get_sheet():
+    sheet = combobox.get()
+    return sheet
+
+
+def get_table_data(book):
+    active_sheet = book.active # stocam in ative_sheet, sheet-ul activ
+    print(type(active_sheet)) # ne returneaza tipul
+    return active_sheet
+    # data_cells = active_sheet["A4":"D5"]
+    # for row in data_cells:
+    #     data_array.append([cell_data.value for cell_data in row])
+
+    # return data_array
+
+
+def print_data(data):
+    print(data)
+
+
+def print_sheet(sheet):
+    print(sheet)
 
 def main():
     path = parameters_selected_btn_clb()
-    print(path)
     book = open_xlsx(path)
-    print(book)
-    combobox.config(values=book.sheetnames)
+    sheet = get_sheet()
+    email = email_emisor()
+    password = password_emisor()
+    data = get_table_data(book)
+    print_data(data)
+    
 
 # -----------------------------------------------------------------
 
@@ -71,6 +111,7 @@ sheet_label.place(x=50, y=70)
 
 combobox = Combobox(root)
 combobox.place(x=90, y=70)
+combobox.set("SheetName")
 
 
 email_label = Label(root, text="Email")
@@ -91,6 +132,9 @@ input_password.place(x=130, y=200)
 
 file_select_btn = Button(root, text="Select file", command=file_select_btn_clb)
 file_select_btn.place(x=400, y=20)
+
+load_sheets = Button(root, text="Load Sheets", command=load_sheets_clb)
+load_sheets.place(x=250, y=65)
 
 parameters_selected = Button(root, text="Get the parameters", command=main)
 parameters_selected.place(x=50, y=300)
